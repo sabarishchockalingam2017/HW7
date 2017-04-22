4f0348fc6b794166d2889121b473c0fc402f32f7
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2017-04-22 12:57:32 -0500

    printing values

---
+++ IMU.X/main.c
@@ -0,0 +1,118 @@
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include "ILI9163C.h"
+
+// PIC32MX250F128B Configuration Bit Settings
+
+// 'C' source line config statements
+// DEVCFG0
+#pragma config DEBUG = 0b11 // no debugging
+#pragma config JTAGEN = OFF // no jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // no write protect
+#pragma config BWP = OFF // no boot write protect
+#pragma config CP = OFF // no code protect
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL // use primary oscillator with pll
+#pragma config FSOSCEN = OFF // turn off secondary oscillator
+#pragma config IESO = OFF // no switching clocks
+#pragma config POSCMOD = HS // high speed crystal mode
+#pragma config OSCIOFNC = OFF // disable secondary osc
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // do not enable clock switch
+#pragma config WDTPS = PS1048576 // use slowest wdt
+#pragma config WINDIS = OFF // wdt no window mode
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+#pragma config UPLLIDIV = DIV_12 // divider for the 8MHz input clock, then multiplied by 12 to get 48MHz for USB
+#pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+#pragma config USERID = 2017 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+#pragma config FUSBIDIO = ON // USB pins controlled by USB module
+#pragma config FVBUSONIO = ON // USB BUSON controlled by USB module
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+void delay(void);
+
+int main() {
+
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    SPI1_init();
+    i2c_master_setup();
+    LCD_init();
+    TRISBbits.TRISB4=1; //making B4 into PORT for usrbutton
+    TRISAbits.TRISA4=0; //making A4 into LAT for LED output
+    LATAbits.LATA4=1;
+    __builtin_enable_interrupts();
+    
+
+    
+    LCD_clearScreen(0x00FF);
+    char buffer[50];
+    sprintf(buffer,"This works!");
+    LCD_writeString(28,20,0xFC00,buffer);
+    char whoami=I2C_read(0x0F);
+    char datap[6];
+    sprintf(buffer,"WHOAMI is: %d",whoami);
+    LCD_writeString(28,32,0xFC00,buffer);
+    sprintf(buffer,"X XL L: ");
+    LCD_writeString(5,40,0xFC00,buffer);
+    sprintf(buffer,"X XL H: ");
+    LCD_writeString(5,50,0xFC00,buffer);
+    sprintf(buffer,"Y XL L: ");
+    LCD_writeString(5,60,0xFC00,buffer);
+    sprintf(buffer,"Y XL H: ");
+    LCD_writeString(5,70,0xFC00,buffer);
+    sprintf(buffer,"Z XL L: ");
+    LCD_writeString(5,80,0xFC00,buffer);
+    sprintf(buffer,"Z XL H: ");
+    LCD_writeString(5,90,0xFC00,buffer);
+    short comb;
+    int i;
+    while(1) {
+//        LATBbits.LATB7=!PORTBbits.RB4;
+        _CP0_SET_COUNT(0);
+        while(_CP0_GET_COUNT()<4800000){
+            ;
+            while(!PORTBbits.RB4) {
+                  ;   // Pin B4 is the USER switch, low (FALSE) if pressed.
+             }
+        }
+        I2C_read_multiple(0x28, 1, datap, 6);
+
+        for(i=0;i<3;i++){
+            comb=combine(datap[2*i],datap[2*i+1]);
+           sprintf(buffer,"%08d",comb);
+           LCD_writeString(50,40+i*10,0xFC00,buffer);
+        }
+        LATAINV=0b10000;
+        
+	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+		  // remember the core timer runs at half the sysclk
+    }
+    return 0;
+}
